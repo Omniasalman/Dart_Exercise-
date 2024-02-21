9318dae4f7fb89fd6a9c2be1c9a139c91762d558
omnia salman <omniasalman928@gmail.com>
2024-02-21 14:01:25 +0200

    Ex4

---
+++ Ex4.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main() {
+  stdout.write("Please choose a number = ");
+  int num = int.parse(stdin.readLineSync()!);
+  for (var i = 1; i <= num; i++) {
+    if (num % i == 2) {
+      print(i);
+    }
+  }
+}
