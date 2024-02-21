53f7207c1964ac16b11cea0cfe352fbe9ce55e4a
omnia salman <omniasalman928@gmail.com>
2024-02-21 13:36:55 +0200

    Ex4

--- Ex1.dart
+++ Ex1.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
-
 void main() {
+ //asks the user to enter their name and their age 
   stdout.write("What'\s Your name? ");
   String name = stdin.readLineSync()!;
   print("Hi , $name ! What is your age ?");
--- Ex2.dart
+++ Ex2.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
-
 void main() {
+  //Ask the user for a number. Depending on whether the number is even or odd
   stdout.write("Enter a randoum number = ");
   int number = int.parse(stdin.readLineSync()!);
   if (number % 2 == 0) {
