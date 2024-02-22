fb692a1e1e2cca15b770f4c5717eae93bae902aa
omnia salman <omniasalman928@gmail.com>
2024-02-22 23:40:04 +0200

    add description comment

--- Ex7.dart
+++ Ex7.dart
@@ -1,4 +1,5 @@
 void main() {
+ // Write a Dart code that takes this list and makes a new list that has only the even elements of this list in it.
   List<int> a = [1, 4, 9, 12, 16, 25, 36, 49, 64, 81, 100];
 
   int i = 0;
