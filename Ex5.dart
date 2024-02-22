fa14fc30641f4026e65ad4c4c1139bca4066437b
omnia salman <omniasalman928@gmail.com>
2024-02-22 13:09:14 +0200

    Update the code

---
+++ Ex5.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main() {
+  List<int> list1 = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+  List<int> list2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+  Set<int> c = {};
+  for (var i; i < list1.length; i++) {
+    for (var n; i < list2.length; n++) {
+      if (list1[i] == list2[n]) {
+        c.add(i);
+      }
+    }
+  }
+  print(c.toList());
+}
---
+++ Ex6.dart
@@ -0,0 +1,7 @@
+import 'dart:io';
+
+void main() {
+  stdout.write("Is a palindrome or not ?");
+  String string = stdin.readLineSync()!;
+  
+}
