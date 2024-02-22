6127eff0304aab0773b77209a31897dccee27cc3
omnia salman <omniasalman928@gmail.com>
2024-02-22 13:26:30 +0200

    Ex number 6

--- Ex6.dart
+++ Ex6.dart
@@ -1,7 +1,14 @@
 import 'dart:io';
 
 void main() {
+
+  //Ask the user for a string and print out whether this string is a palindrome or not.
+  //A palindrome is a string that reads the same forwards and backwards.
   stdout.write("Is a palindrome or not ?");
-  String string = stdin.readLineSync()!;
-  
+  String input = stdin.readLineSync()!.toLowerCase();
+  String revInput = input.split('').reversed.join('');
+
+  input == revInput
+      ? print("The word is palindrome")
+      : print("The world is not a palindrome");
 }
