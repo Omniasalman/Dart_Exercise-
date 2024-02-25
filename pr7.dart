da89078021623731ee67d4f91c73407c466cc1fd
omnia salman <omniasalman928@gmail.com>
2024-02-25 23:26:17 +0200

    problem solving number 7

---
+++ pr7.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+void main() {
+  stdout.write("Enter the number of the day per week ");
+  int day = int.parse(stdin.readLineSync() ?? "0");
+  switch (day) {
+    case 1:
+      print("Sunday");
+      break;
+    case 2:
+      print("Monday");
+      break;
+    case 3:
+      print("Tuesday");
+      break;
+    case 4:
+      print("Wednesday ");
+      break;
+    case 5:
+      print("Tuesday");
+      break;
+    case 6:
+      print("Friday");
+      break;
+    case 7:
+      print("Suterday");
+      break;
+  }
+}
