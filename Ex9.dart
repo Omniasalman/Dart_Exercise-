4e736759f86551d5ddb2386200a7454bb1d0a5f4
omnia salman <omniasalman928@gmail.com>
2024-02-25 12:06:56 +0200

    Ex9

--- Ex9.dart
+++ Ex9.dart
@@ -9,7 +9,12 @@ void main() {
   if (num1 == num2) {
     print("number1 and number2 are equal");
   }
-
-
-  
+ //////////////////////////////////
+  stdout.write("Enter rundom number");
+  int? rund = int.parse(stdin.readLineSync() ?? "0");
+  if (rund % 2 == 0) {
+    print("The rundom number you enter is Even");
+  } else if (rund % 2 != 0) {
+    print("The rnumdom number is odd");
+  }
 }
