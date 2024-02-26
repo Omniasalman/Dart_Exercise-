41fa4b51dd4c29620d8b3becbcadc73d45e1d33a
omnia salman <omniasalman928@gmail.com>
2024-02-27 00:38:51 +0200

    Update

--- Ex4.dart
+++ Ex4.dart
@@ -8,4 +8,14 @@ void main() {
       print(i);
     }
   }
+  int sum = 0;
+  double average = 0.0;
+  for (int i = 1; i < 10; i++) {
+    stdout.write("Number $i :");
+    int number = int.parse(stdin.readLineSync() ?? "0");
+    sum += number;
+  }
+  print("The sum of the 10 integer $sum =");
+  average = sum / 10;
+  print("the average  = $average");
 }
